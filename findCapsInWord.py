b12ba15bf81fb848d8d7ebbc84d9e798b296825c
satrakidol <geotakidis@gmail.com>
2024-03-22 19:30:53 +0200

    2nd

--- findCapsInWord.py
+++ findCapsInWord.py
@@ -1,12 +1,22 @@
-def capital_indexes(word: str):
-    myList = []
+def capital_letters(word: str):
+    Caps = []
     for i in word:
         if i.isupper():
-            myList.append(i)
-    return myList
+            Caps.append(i)
+    return Caps
+
+def capital_indexes(word: str):
+    indexes = []
+    for i in range(len(word)):
+        if word[i].isupper():
+            indexes.append(i)
+    return indexes
+
 
 word = input('Please type a word: ')
 
-Caps = capital_indexes(word)
+Caps = capital_letters(word)
 
-print(Caps)
+indexes = capital_indexes(word)
+
+print(' The capital letters are + ', Caps, 'and they are located at ',indexes)
